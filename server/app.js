04a3607ecb7c536409919caab55aaa7d3ac0a170
amirmub <amirmubarek01@gmail.com>
2025-05-20 21:26:07 +0300

    add all routes

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 const express = require("express")
 
 const app = express();
+// listening port number
 const port = 5500;
 
 const dbConnection = require("./db/db.config")
 
-app.get("/",(req,res) =>{
-    res.send("test")
+// register route
+app.post("/api/users/register",(req,res) =>{
+    res.status(201).json({msg : "register" })
+})
+
+// login route
+app.post("/api/users/login",(req,res) =>{
+    res.status(201).json({msg : "login" })
+})
+
+// userCheck route
+app.get("/api/users/check",(req,res) =>{
+    res.status(201).json({msg : "check" })
 })
 
 app.listen(port,(err) =>{
