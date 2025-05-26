69ed0185446567b09ba4b6d12d95073eef0c78f6
amirmub <amirmubarek01@gmail.com>
2025-05-26 15:51:44 +0300

    add register component

--- client/forum/src/components/Register/Register.jsx
+++ client/forum/src/components/Register/Register.jsx
@@ -1,10 +1,35 @@
-
 function Register() {
   return (
     <div>
-      Register
+      <form action="">
+        <div>
+          <label htmlFor="">username :--</label>
+          <input type="text" placeholder="username" />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="">firstname :--</label>
+          <input type="text"  placeholder="firstname"/>
+        </div>
+        <br />
+        <div>
+          <label htmlFor="">lastname :--</label>
+          <input type="text" placeholder="lastname"/>
+        </div>
+        <br />
+        <div>
+          <label htmlFor="">email :--</label>
+          <input type="email" placeholder="email"/>
+        </div>
+        <br />
+        <div>
+          <label htmlFor="">password :--</label>
+          <input type="password" placeholder="password"/>
+        </div>
+        <button>Register</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
