b9a812aef9c77dbfc9063e66d161011c133a368d
amirmub <amirmubarek01@gmail.com>
2025-05-26 16:26:35 +0300

    feat: implement registration functionality with form

--- client/forum/src/components/Register/Register.jsx
+++ client/forum/src/components/Register/Register.jsx
@@ -1,30 +1,70 @@
+import { useRef } from "react";
+import axios from "../../utills/axios";
+import {useNavigate} from "react-router-dom"
+
+
 function Register() {
+  const navigate = useNavigate();
+
+  const usernameDom = useRef();
+  const firstnameDom = useRef();
+  const lastnameDom = useRef();
+  const emailDom = useRef();
+  const passwordDom = useRef();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const usernameValue = usernameDom.current.value;
+    const firstnameValue = firstnameDom.current.value;
+    const lastnameValue = lastnameDom.current.value;
+    const emailValue = emailDom.current.value;
+    const passwordValue = passwordDom.current.value;
+
+    try {
+      const response = await axios.post("/users/register",
+        {
+        username : usernameValue,
+        firstname : firstnameValue,
+        lastname : lastnameValue,
+        email : emailValue,
+        password : passwordValue
+      });
+
+      // console.log(response);
+      navigate("/");
+      
+    } catch (error) {
+      console.log(error.response);
+      
+    }
+  }
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="">username :--</label>
-          <input type="text" placeholder="username" />
+          <input ref={usernameDom} type="text" placeholder="username" />
         </div>
         <br />
         <div>
           <label htmlFor="">firstname :--</label>
-          <input type="text"  placeholder="firstname"/>
+          <input ref={firstnameDom} type="text" placeholder="firstname" />
         </div>
         <br />
         <div>
           <label htmlFor="">lastname :--</label>
-          <input type="text" placeholder="lastname"/>
+          <input ref={lastnameDom} type="text" placeholder="lastname" />
         </div>
         <br />
         <div>
           <label htmlFor="">email :--</label>
-          <input type="email" placeholder="email"/>
+          <input ref={emailDom} type="email" placeholder="email" />
         </div>
         <br />
         <div>
           <label htmlFor="">password :--</label>
-          <input type="password" placeholder="password"/>
+          <input ref={passwordDom} type="password" placeholder="password" />
         </div>
         <button>Register</button>
       </form>
